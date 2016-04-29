45a16c5cde80cde69430dbef6b1336a717c7c330
rkalgasi <rkalgasi@gmail.com>
2016-04-29 18:56:04 +0700

    change login style

--- app/src/main/java/com/rzk/android_monitor_app/LoginActivity.java
+++ app/src/main/java/com/rzk/android_monitor_app/LoginActivity.java
@@ -2,12 +2,21 @@ package com.rzk.android_monitor_app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+    EditText email, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        email = (EditText) findViewById(R.id.input_email);
+        password = (EditText) findViewById(R.id.input_password);
+    }
+
+    public void login(View view) {
+        //create login process
     }
 }
