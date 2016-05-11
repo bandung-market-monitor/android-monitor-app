398ccf83ac62bc2d5c6d0a621b4a0c436b1e2a6d
rkalgasi <rkalgasi@gmail.com>
2016-05-12 06:34:49 +0700

    Change EditText to TextInputEditText in login and input activity

--- app/src/main/java/com/rzk/android_monitor_app/LoginActivity.java
+++ app/src/main/java/com/rzk/android_monitor_app/LoginActivity.java
@@ -1,20 +1,20 @@
 package com.rzk.android_monitor_app;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TextInputEditText;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
-    EditText email, password;
+    TextInputEditText email, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        email = (EditText) findViewById(R.id.input_email);
-        password = (EditText) findViewById(R.id.input_password);
+        email = (TextInputEditText) findViewById(R.id.input_email);
+        password = (TextInputEditText) findViewById(R.id.input_password);
     }
 
     public void login(View view) {
